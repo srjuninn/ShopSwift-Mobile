25494fe5f22b05cb2649e4adef24eab05268a423
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-08-25 10:52:37 -0300

    style: add sign up

---
+++ src/screens/signUp/style.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+    containerHome: {
+        paddingHorizontal: 20,
+        gap: 78
+    },
+    titleTxt: {
+        color: '#2D3133',
+        fontSize: 40,
+        fontWeight: '700',
+        width: 300
+    },
+    titleHighlights: {
+        color: '#375EE7',
+        fontSize: 40
+    },
+    formContainer: {
+        gap: 20
+    },
+    contentSignUp: {
+        gap: 20
+    },
+    txtInput: {
+        fontSize: 24,
+        color: '#000000'
+    },
+    input: {
+        borderWidth: 1,
+        borderRadius: 8,
+        paddingHorizontal: 10,
+        borderColor: '#2D3133'
+    },
+    button: {
+        marginTop: 40,
+        paddingHorizontal: 50,
+        paddingVertical: 18,
+        backgroundColor: '#375EE7',
+        borderRadius: 8
+    },
+    buttonTxt: {
+        textAlign: 'center',
+        color: '#F8FAFC',
+        fontSize: 24
+    },
+    haveAccountTxt: {
+        textAlign: 'center',
+        fontSize: 18
+    },
+    haveAccountSignUp: {
+        color: '#375EE7',
+        fontWeight: '700'
+    }
+
+})
