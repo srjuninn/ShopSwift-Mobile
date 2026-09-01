39fbd9f2891154ecc15c9c546da40434985058af
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-09-01 09:15:29 -0300

    style: add styled component sign up

---
+++ src/components/Cadastro/style.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+   padding: 0 20px;
+   gap: 78px;
+ `
+
+export const TextTitle = styled.Text`
+    font-size: 40px;
+    font-weight: 700;
+    width: 290px;
+ `
+
+export const TextHighlights = styled.Text`
+    color: #375EE7;
+ `
+
+export const FormContainer = styled.View`
+   gap: 20px;
+ `
+
+export const FormLabel = styled.View`
+   gap: 20px;
+ `
+
+export const TextLabel = styled.Text`
+   font-size: 28px;
+`
+
+export const Input = styled.TextInput`
+   padding: 10px 20px;
+   border: 1px solid #2D3133;
+   border-radius: 7px;
+   font-size: 24px;
+   `
+
+export const SignUpButton = styled.TouchableOpacity`
+   padding: 18px 50px;
+   background-color: #375EE7;
+   border-radius: 7px;
+   `
+
+export const SignUpButtonText = styled.Text`
+   font-size: 24px;
+   color: #F8FAFC;
+   text-align: center;
+   `
+export const HaveAnAccount = styled.View`
+   gap: 20px;
+`
+
+export const HaveAnAccountText = styled.Text`
+   font-size: 24px;
+   text-align: center;
+   `
+
+export const SignUpText = styled.Text`
+   font-size: 24px;
+   font-weight: bold;
+   text-align: center;
+   color: #375EE7;
+   text-decoration: underline;
+`
