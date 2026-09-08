718b41e0c80e6940a88f6072de49d30d345c6d91
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-09-08 08:43:30 -0300

    style: add category

--- src/screens/home/style.js
+++ src/screens/home/style.js
@@ -1,7 +1,6 @@
 import styled from "styled-components/native";
 
 export const HomeContainer = styled.View`
-    border: 1px solid black;
     gap: 78px;
     padding-left: 20px;
 `
@@ -23,3 +22,40 @@ export const ScrollViewHighlights = styled.ScrollView.attrs({
         gap: 10
     }
 })``
+
+export const OurCategories = styled.View`
+    gap: 12px;
+    `
+
+export const OurCategoriesTitle = styled.Text`
+    font-size: 25px;
+    font-weight: 700;
+    `
+
+export const OurCategoriesText = styled.Text`
+    color: #375EE7;
+    `
+
+export const Categories = styled.View`
+    flex-direction: row;
+    gap: 10px;
+    `
+
+export const Category = styled.View`
+    flex-direction: row;
+    padding: 10px 20px; 
+    gap: 12px;
+    border: 1px solid #375EE7;
+    border-radius: 7px;
+`
+
+export const CategoryImage = styled.Image`
+    width: 30px;
+    height: 30px;
+`
+
+export const CategoryText = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+    color: #375EE7
+`
