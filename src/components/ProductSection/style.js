07953abceac951817bfa2d0f89f02961d7302b79
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-09-15 09:00:14 -0300

    style: add product section

---
+++ src/components/ProductSection/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+
+export const BoxProducts = styled.View`
+    margin-top: 44px;
+    gap: 10px;
+`
+
+export const TextTitle = styled.Text`
+    font-size: 25px;
+    font-weight: 700;
+`
+
+export const TextHighlight = styled.Text`
+    color: #375EE7;
+`
+
+export const ContainerProducts = styled.View`
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 10px;
+`
